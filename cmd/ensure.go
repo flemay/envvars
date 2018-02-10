12170fbca917925094c8ecad5df0ae10bdd2c822
Frederic Lemay <flemay.me@gmail.com>
2018-02-10 19:48:48 +1100

    func to combine NewDefinition and Validate

--- cmd/ensure.go
+++ cmd/ensure.go
@@ -9,11 +9,8 @@ var ensureCmd = &cobra.Command{
 	Use:   "ensure",
 	Short: "Verify that your environment variables comply to their definition",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		definition, err := envvars.NewDefinition(definitionFileRootFlag)
+		definition, err := envvars.NewDefinitionAndValidate(definitionFileRootFlag)
 		if err != nil {
-			return err
-		}
-		if err := envvars.Validate(definition); err != nil {
 			return err
 		}
 		return envvars.Ensure(definition)
--- pkg/envvars/definition_test.go
+++ pkg/envvars/definition_test.go
@@ -3,30 +3,97 @@ package envvars_test
 import (
 	"github.com/flemay/envvars/pkg/envvars"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"testing"
 )
 
-func TestNewDefinition_toReturnTheContentOfValidEnvvarsFile(t *testing.T) {
+func TestNewDefinition_toReturnDefinitionBasedOnEnvvarsFile(t *testing.T) {
 	// given
 	envvarsFilePath := "testdata/envvars.toml"
 
 	// when
-	m, err := envvars.NewDefinition(envvarsFilePath)
+	definition, err := envvars.NewDefinition(envvarsFilePath)
 
 	// then
 	assert.NoError(t, err)
-	assert.NotNil(t, m)
-	assert.Len(t, m.Envvars, 2)
+	assert.NotNil(t, definition)
+	assert.Len(t, definition.Envvars, 2)
 }
 func TestNewDefinition_toReturnErrorIfMalformatedEnvvarsFile(t *testing.T) {
 	// given
 	envvarsFilePath := "testdata/malformated_envvars.toml"
 
 	// when
-	m, err := envvars.NewDefinition(envvarsFilePath)
+	definition, err := envvars.NewDefinition(envvarsFilePath)
 
 	// then
 	assert.Error(t, err)
-	assert.Nil(t, m)
+	assert.Nil(t, definition)
 	assert.Contains(t, err.Error(), "error occurred when opening the file "+envvarsFilePath)
+}
+
+func TestNewDefinition_toReturnErrorIfFileNotFound(t *testing.T) {
+	// given
+	noSuchFilePath := "nosuchfile.toml"
+
+	// when
+	definition, err := envvars.NewDefinition(noSuchFilePath)
+
+	// then
+	assert.Error(t, err)
+	assert.Nil(t, definition)
+	assert.Contains(t, err.Error(), "open nosuchfile.toml: no such file or directory")
+}
+
+func TestNewDefinitionAndValidate_toReturnDefinitionBasedOnValidEnvvarsFile(t *testing.T) {
+	// given
+	envvarsFilePath := "testdata/envvars.toml"
+
+	// when
+	definition, err := envvars.NewDefinitionAndValidate(envvarsFilePath)
+
+	// then
+	assert.NoError(t, err)
+	assert.NotNil(t, definition)
+	assert.Len(t, definition.Envvars, 2)
+}
+
+func TestNewDefinitionAndValidate_toReturnErrorIfInvalidEnvvarsFile(t *testing.T) {
+	// given
+	envvarsFilePath := "testdata/invalid_envvars.toml"
+
+	// when
+	definition, err := envvars.NewDefinitionAndValidate(envvarsFilePath)
+
+	// then
+	assert.Error(t, err)
+	assert.NotNil(t, definition)
+	expectedErrorMsg, _ := ioutil.ReadFile("testdata/validate_error_message.golden")
+	assert.EqualError(t, err, string(expectedErrorMsg))
+}
+
+func TestNewDefinitionAndValidate_toReturnErrorIfMalformatedEnvvarsFile(t *testing.T) {
+	// given
+	envvarsFilePath := "testdata/malformated_envvars.toml"
+
+	// when
+	definition, err := envvars.NewDefinitionAndValidate(envvarsFilePath)
+
+	// then
+	assert.Error(t, err)
+	assert.Nil(t, definition)
+	assert.Contains(t, err.Error(), "error occurred when opening the file "+envvarsFilePath)
+}
+
+func TestNewDefinitionAndValidate_toReturnErrorIfFileNotFound(t *testing.T) {
+	// given
+	noSuchFilePath := "nosuchfile.toml"
+
+	// when
+	definition, err := envvars.NewDefinitionAndValidate(noSuchFilePath)
+
+	// then
+	assert.Error(t, err)
+	assert.Nil(t, definition)
+	assert.Contains(t, err.Error(), "open nosuchfile.toml: no such file or directory")
 }
