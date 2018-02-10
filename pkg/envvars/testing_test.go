da8697becb95715a524684b311171c19169dce66
flemay <461102+flemay@users.noreply.github.com>
2018-02-10 23:07:58 +1100

    generate dotenv file

--- pkg/envvars/testing_test.go
+++ pkg/envvars/testing_test.go
@@ -15,7 +15,7 @@ func givenDefinition(t *testing.T, name string) *envvars.Definition {
 	return definition
 }
 
-func removeFile(t *testing.T, name string) {
+func removeFileOrDir(t *testing.T, name string) {
 	if err := os.Remove(name); err != nil {
 		t.Fatalf(err.Error())
 	}
@@ -27,4 +27,18 @@ func readFile(t *testing.T, name string) string {
 		t.Fatalf(err.Error())
 	}
 	return string(f)
+}
+
+func createEmptyFile(t *testing.T, name string) {
+	f, err := os.Create(name)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if err := f.Close(); err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func createDir(t *testing.T, name string) {
+	os.Mkdir(name, os.ModePerm)
 }
