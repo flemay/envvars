bd25a0afdaa8227ec5db834cdc3efaea8be89bd2
flemay <461102+flemay@users.noreply.github.com>
2018-02-16 18:01:16 +1100

    more text

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # envvars
 
 envvars gives your environment variables the love they deserve.
+
+## Installation
+
+## Usage
+
+```bash
+# create a definition file envvars.toml
+# [[envvars]]
+#   name="ECHO"
+#   desc="env var ECHO"
+$ printf "[[envvars]]\n  name=\"ECHO\"\n  desc=\"env var ECHO\"" > envvars.toml
+
+# validate the definition file if it contains errors
+$ envvars validate
+
+# ensure the environment variables comply with the definition file
+$ envvars ensure
+# Error: environment variable ECHO must be set
+# set ECHO
+$ export ECHO="helloworld"
+$ envvars ensure
+# create an env file
+$ envvars envfile
+$ cat .env
+# ECHO
+
+# explore
+$ envvars --help
+```
--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -12,6 +12,13 @@ var tagsRootFlag []string
 var rootCmd = &cobra.Command{
 	Use:   "envvars",
 	Short: "envvars gives your environment variables the love they deserve",
+	Long: `A way to define environment variables and ensure they comply
+Usage examples
+  validate the definition file if it contains errors
+    $ envvars validate
+  ensure the environment variables comply with the definition file
+    $ envvars ensure`,
+	SilenceUsage: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
@@ -19,12 +26,11 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&definitionFileRootFlag, "file", "f", "envvars.toml", "definition file")
-	rootCmd.PersistentFlags().StringSliceVarP(&tagsRootFlag, "tags", "t", nil, "execute subcommands against environment variables that have the tags (ex: --tags test,build)")
+	rootCmd.PersistentFlags().StringSliceVarP(&tagsRootFlag, "tags", "t", nil, "execute subcommands against environment variables with the tags (ex: --tags test,build)")
 }
