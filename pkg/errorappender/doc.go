6458aec28d868c5661f7a6703b1c3948922f508e
flemay <461102+flemay@users.noreply.github.com>
2018-04-25 12:46:42 +1000

    add doc.go files for each package

---
+++ pkg/errorappender/doc.go
@@ -0,0 +1,4 @@
+/*
+Package errorappender appends errors and error strings in a single error.
+*/
+package errorappender
---
+++ pkg/yml/doc.go
@@ -0,0 +1,4 @@
+/*
+Package yml reads a Declaration File in YML format.
+*/
+package yml
