4c5e07e728ed73f5dcd0bc63db2739f0376d4add
flemay <461102+flemay@users.noreply.github.com>
2021-09-01 21:35:22 +1000

    replace testify

--- pkg/envvars/ensure_test.go
+++ pkg/envvars/ensure_test.go
@@ -1,26 +1,25 @@
 package envvars_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/flemay/envvars/pkg/envvars"
-	"github.com/flemay/envvars/pkg/mocks"
 	"github.com/flemay/envvars/pkg/yml"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestEnsure_toReturnErrorIfInvalidDeclarationAndTagNameList(t *testing.T) {
 	// given
 	reader := yml.NewDeclarationYML("testdata/declaration_file_invalid.yml")
-	invalidList := givenInvalidTagNameList()
+	invalidTags := []string{"tagNotThere", "tagDuplicated", "tagDuplicated", ""}
 
 	// when
-	err := envvars.Ensure(reader, invalidList...)
+	got := envvars.Ensure(reader, invalidTags...)
 
 	// then
-	expectedErrorMsg := readFile(t, "testdata/declaration_file_with_tag_name_list_invalid_error_message.golden")
-	assert.EqualError(t, err, expectedErrorMsg)
+	want := readFile(t, "testdata/declaration_file_with_tag_name_list_invalid_error_message.golden")
+	if got.Error() != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
 }
 
 func TestEnsure_toReturnNoErrorIfEnvvarsComply(t *testing.T) {
@@ -29,90 +28,101 @@ func TestEnsure_toReturnNoErrorIfEnvvarsComply(t *testing.T) {
 	defer helperSetenv(t,
 		keyValue{"ENVVAR_1", "name1"},
 		keyValue{"ENVVAR_2", "name2"},
-		keyValue{"ENVVAR_3", "name3"})()
+		keyValue{"ENVVAR_3", "name3"},
+		keyValue{"OPTIONAL_ENVVAR", "optional"},
+	)()
 
 	// when
 	err := envvars.Ensure(reader)
 
 	// then
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("want no error, got %q", err.Error())
+	}
 }
 
 func TestEnsure_toReturnNoErrorIfOptionalEnvvarIsNotDefined(t *testing.T) {
 	// given
-	d := &envvars.Declaration{
-		Envvars: []*envvars.Envvar{
-			&envvars.Envvar{
-				Name:     "NAME",
-				Desc:     "Desc",
-				Optional: true,
-			},
-		},
-	}
-	mockReader := new(mocks.DeclarationReader)
-	mockReader.On("Read").Return(d, nil)
+	reader := yml.NewDeclarationYML("testdata/ensure_declaration_file.yml")
+	defer helperSetenv(t,
+		keyValue{"ENVVAR_1", "name1"},
+		keyValue{"ENVVAR_2", "name2"},
+		keyValue{"ENVVAR_3", "name3"},
+	)()
 
 	// when
-	err := envvars.Ensure(mockReader)
+	got := envvars.Ensure(reader)
 
 	// then
-	assert.NoError(t, err)
+	if got != nil {
+		t.Errorf("want no error, got %q", got.Error())
+	}
 }
 
 func TestEnsure_toReturnNoErrorIfOptionalEnvvarHasEmptyValue(t *testing.T) {
-	// given
-	d := &envvars.Declaration{
-		Envvars: []*envvars.Envvar{
-			&envvars.Envvar{
-				Name:     "NAME",
-				Desc:     "Desc",
-				Optional: true,
-			},
-		},
-	}
-	mockReader := new(mocks.DeclarationReader)
-	mockReader.On("Read").Return(d, nil)
-	os.Setenv("NAME", "")
+	reader := yml.NewDeclarationYML("testdata/ensure_declaration_file.yml")
+	defer helperSetenv(t,
+		keyValue{"ENVVAR_1", "name1"},
+		keyValue{"ENVVAR_2", "name2"},
+		keyValue{"ENVVAR_3", "name3"},
+		keyValue{"OPTIONAL_ENVVAR", ""},
+	)()
 
 	// when
-	err := envvars.Ensure(mockReader)
+	err := envvars.Ensure(reader)
 
 	// then
-	os.Unsetenv("NAME")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Errorf("want no error, got %q", err.Error())
+	}
 }
 
 func TestEnsure_toReturnErrorIfEnvvarsDoNotComply(t *testing.T) {
 	// given
 	reader := yml.NewDeclarationYML("testdata/ensure_declaration_file.yml")
-	os.Setenv("ENVVAR_2", "")
-	defer os.Unsetenv("ENVVAR_2")
+	defer helperSetenv(t,
+		keyValue{"ENVVAR_2", ""},
+	)()
 
 	// when
 	got := envvars.Ensure(reader)
 
 	// then
 	want := readFile(t, "testdata/ensure_error_message.golden")
-	assert.EqualError(t, got, want)
+	if got.Error() != want {
+		t.Errorf("want %q, got %q", want, got.Error())
+	}
 }
 
 func TestEnsure_toReturnNoErrorIfTaggedEnvvarsComply(t *testing.T) {
 	// given
 	reader := yml.NewDeclarationYML("testdata/ensure_declaration_file.yml")
-	os.Setenv("ENVVAR_2", "name2")
+	defer helperSetenv(t,
+		keyValue{"ENVVAR_2", "name2"},
+	)()
+
 	// when
-	err := envvars.Ensure(reader, "tag2")
+	got := envvars.Ensure(reader, "tag2")
+
 	// then
-	assert.NoError(t, err)
-	os.Unsetenv("ENVVAR_2")
+	if got != nil {
+		t.Errorf("want no error, got %q", got.Error())
+	}
 }
 
 func TestEnsure_toReturnErrorIfTaggedEnvvarsDoNotComply(t *testing.T) {
 	// given
 	reader := yml.NewDeclarationYML("testdata/ensure_declaration_file.yml")
-	os.Setenv("ENVVAR_2", "name2")
+	defer helperSetenv(t,
+		keyValue{"ENVVAR_2", "name2"},
+	)()
+
 	// when
-	err := envvars.Ensure(reader, "tag1")
+	got := envvars.Ensure(reader, "tag1")
+
 	// then
-	assert.EqualError(t, err, "environment variable ENVVAR_1 is not defined. Variable description: Desc of ENVVAR_1")
+	want := "environment variable ENVVAR_1 is not defined. Variable description: Desc of ENVVAR_1"
+	if got.Error() != want {
+		t.Errorf("want %q, got %q", want, got.Error())
+	}
 }
