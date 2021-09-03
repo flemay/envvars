16fdf959ebeba361e95915d3e1204b354ddf304f
flemay <461102+flemay@users.noreply.github.com>
2021-09-03 23:35:02 +1000

    replace testify for declaration_test.go

--- pkg/envvars/declaration_test.go
+++ pkg/envvars/declaration_test.go
@@ -1,25 +1,31 @@
 package envvars_test
 
 import (
-	"github.com/flemay/envvars/pkg/envvars"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/flemay/envvars/pkg/envvars"
 )
 
-func TestEnvvar_HasTag_toReturnTrueIfTagIsPresent(t *testing.T) {
-	// given
-	ev := envvars.Envvar{Tags: []string{"T1", "T2"}}
-	// when
-	hasTag := ev.HasTag("T1")
-	// then
-	assert.True(t, hasTag)
-}
+func TestEnvvarHasTag(t *testing.T) {
+	testCases := map[string]struct {
+		givenTags  []string
+		whenTag    string
+		thenHasTag bool
+	}{
+		"true if tag is present":      {[]string{"T1", "T2"}, "T1", true},
+		"false if tag is not present": {[]string{"T1", "T2"}, "T3", false},
+	}
 
-func TestEnvvar_HasTag_toReturnFalseIfTagIsNotPresent(t *testing.T) {
-	// given
-	ev := envvars.Envvar{Tags: []string{"T1", "T2"}}
-	// when
-	hasTag := ev.HasTag("tagNotThere")
-	// then
-	assert.False(t, hasTag)
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// given
+			ev := envvars.Envvar{Tags: tc.givenTags}
+			// when
+			got := ev.HasTag(tc.whenTag)
+			// then
+			if got != tc.thenHasTag {
+				t.Errorf("want %t, got %t", tc.thenHasTag, got)
+			}
+		})
+	}
 }
