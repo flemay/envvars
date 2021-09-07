a0f89bf0c2481290a2c2f4f7d4e8691013c5ece5
flemay <461102+flemay@users.noreply.github.com>
2021-09-07 19:56:03 +1000

    replace testify for envfile_test

--- pkg/envvars/envfile_test.go
+++ pkg/envvars/envfile_test.go
@@ -1,25 +1,31 @@
 package envvars_test
 
 import (
+	"testing"
+
+	"github.com/flemay/envvars/pkg/envfile"
 	"github.com/flemay/envvars/pkg/envvars"
 	"github.com/flemay/envvars/pkg/mocks"
 	"github.com/flemay/envvars/pkg/yml"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"testing"
 )
 
 func TestEnvfile_toReturnErrorIfInvalidDeclarationAndTagNameList(t *testing.T) {
 	// given
-	reader := yml.NewDeclarationYML("testdata/declaration_file_invalid.yml")
-	invalidList := givenInvalidTagNameList()
-	mockWriter := new(mocks.EnvfileWriter)
+	r := yml.NewDeclarationYML("testdata/declaration_file_invalid.yml")
+	tags := givenInvalidTagNameList()
+	filename := t.TempDir() + "/.env"
+	w := envfile.NewEnvfile(filename, false, false)
+
 	// when
-	err := envvars.Envfile(reader, mockWriter, invalidList...)
+	got := envvars.Envfile(r, w, tags...)
 
 	// then
-	expectedErrorMsg := readFile(t, "testdata/declaration_file_with_tag_name_list_invalid_error_message.golden")
-	assert.EqualError(t, err, expectedErrorMsg)
+	want := readFile(t, "testdata/declaration_file_with_tag_name_list_invalid_error_message.golden")
+	if got.Error() != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
 }
 
 func TestEnvfile_toWriteEnvfile(t *testing.T) {
