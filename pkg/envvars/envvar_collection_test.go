9ce895b4f6bb4db55d637cbd0adc2bbd8ca5a219
flemay <461102+flemay@users.noreply.github.com>
2021-09-08 20:08:45 +1000

    replace testify

--- pkg/envvars/envvar_collection_test.go
+++ pkg/envvars/envvar_collection_test.go
@@ -1,88 +1,109 @@
 package envvars_test
 
 import (
-	"github.com/flemay/envvars/pkg/envvars"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/flemay/envvars/pkg/envvars"
 )
 
-func TestEnvvarCollection_Get_toReturnEnvvarMatchingName(t *testing.T) {
-	// given
-	c := envvars.EnvvarCollection{
+func TestEnvvarCollectionGet(t *testing.T) {
+	givenEnvvars := envvars.EnvvarCollection{
 		&envvars.Envvar{Name: "ENVVAR_1"},
 		&envvars.Envvar{Name: "ENVVAR_2"},
 	}
-	// when
-	ev := c.Get("ENVVAR_2")
-	// then
-	assert.NotNil(t, ev)
-	assert.Equal(t, "ENVVAR_2", ev.Name)
-}
-
-func TestEnvvarCollection_Get_toReturnNilIfNoEnvvarMatchingName(t *testing.T) {
-	// given
-	c := envvars.EnvvarCollection{
-		&envvars.Envvar{Name: "ENVVAR_1"},
-		&envvars.Envvar{Name: "ENVVAR_2"},
+	testCases := map[string]struct {
+		givenCollection envvars.EnvvarCollection
+		whenEnvvarName  string
+		thenEnvvar      *envvars.Envvar
+	}{
+		"return envvar if match": {givenEnvvars, "ENVVAR_2", givenEnvvars[1]},
+		"return nil if no match": {givenEnvvars, "NOT_DEFINED", nil},
 	}
-	// when
-	ev := c.Get("NOT_DEFINED")
-	// then
-	assert.Nil(t, ev)
-}
 
-func TestEnvvarCollection_GetAll_toReturnEnvvarCollectionMatchingName(t *testing.T) {
-	// given
-	c := envvars.EnvvarCollection{
-		&envvars.Envvar{Name: "ENVVAR_1"},
-		&envvars.Envvar{Name: "ENVVAR_2"},
-		&envvars.Envvar{Name: "ENVVAR_1"},
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// when
+			got := tc.givenCollection.Get(tc.whenEnvvarName)
+			// then
+			if tc.thenEnvvar != nil {
+				if got == nil {
+					t.Error("want an envvar, got nil")
+					return
+				}
+				if got.Name != tc.thenEnvvar.Name {
+					t.Errorf("want envvar with name %q, got %q", tc.thenEnvvar.Name, got.Name)
+				}
+				return
+			}
+			if got != nil {
+				t.Error("want nil, got not nil")
+			}
+		})
 	}
-	// when
-	evs := c.GetAll("ENVVAR_1")
-	// then
-	assert.Len(t, evs, 2)
-	assert.Equal(t, "ENVVAR_1", evs[0].Name)
-	assert.Equal(t, "ENVVAR_1", evs[1].Name)
 }
 
-func TestEnvvarCollection_GetAll_toReturnEmtpyEnvvarCollectionIfNoneMatchingName(t *testing.T) {
-	// given
-	c := envvars.EnvvarCollection{
+func TestEnvvarCollectionGetAll(t *testing.T) {
+	givenEnvvars := envvars.EnvvarCollection{
 		&envvars.Envvar{Name: "ENVVAR_1"},
 		&envvars.Envvar{Name: "ENVVAR_2"},
 		&envvars.Envvar{Name: "ENVVAR_1"},
 	}
-	// when
-	evs := c.GetAll("NOT_DEFINED")
-	// then
-	assert.Len(t, evs, 0)
+	testCases := map[string]struct {
+		givenCollection envvars.EnvvarCollection
+		whenEnvvarName  string
+		thenEnvvars     envvars.EnvvarCollection
+	}{
+		"return matching envvars":             {givenEnvvars, "ENVVAR_1", envvars.EnvvarCollection{givenEnvvars[0], givenEnvvars[2]}},
+		"return empty collection if no match": {givenEnvvars, "NOT_DEFINED", envvars.EnvvarCollection{}},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// when
+			got := tc.givenCollection.GetAll(tc.whenEnvvarName)
+			// then
+			if len(got) != len(tc.thenEnvvars) {
+				t.Errorf("want %q, got %q", len(tc.thenEnvvars), len(got))
+				return
+			}
+			for i, envvar := range got {
+				if envvar.Name != tc.thenEnvvars[i].Name {
+					t.Errorf("want %q, got %q", tc.thenEnvvars[i].Name, envvar.Name)
+				}
+			}
+		})
+	}
 }
 
-func TestEnvvarCollection_WithTag_toReturnEnvvarsWithTagName(t *testing.T) {
-	// given
-	c := envvars.EnvvarCollection{
+func TestEnvvarCollectionWithTag(t *testing.T) {
+	givenEnvvars := envvars.EnvvarCollection{
 		&envvars.Envvar{Name: "ENVVAR_1", Tags: []string{"T1", "T2"}},
 		&envvars.Envvar{Name: "ENVVAR_2", Tags: []string{"T1"}},
 		&envvars.Envvar{Name: "ENVVAR_3", Tags: []string{"T1", "T2"}},
 	}
-	// when
-	evs := c.WithTag("T2")
-	// then
-	assert.Len(t, evs, 2)
-	assert.Equal(t, "ENVVAR_1", evs[0].Name)
-	assert.Equal(t, "ENVVAR_3", evs[1].Name)
-}
-
-func TestEnvvarCollection_WithTag_toReturnEmptyIfNoEnvvarWithTagName(t *testing.T) {
-	// given
-	c := envvars.EnvvarCollection{
-		&envvars.Envvar{Name: "ENVVAR_1", Tags: []string{"T1", "T2"}},
-		&envvars.Envvar{Name: "ENVVAR_2", Tags: []string{"T1"}},
-		&envvars.Envvar{Name: "ENVVAR_3", Tags: []string{"T1", "T2"}},
+	testCases := map[string]struct {
+		givenCollection envvars.EnvvarCollection
+		whenTagName     string
+		thenEnvvars     envvars.EnvvarCollection
+	}{
+		"return envvars with matching tag":    {givenEnvvars, "T2", envvars.EnvvarCollection{givenEnvvars[0], givenEnvvars[2]}},
+		"return empty collection if no match": {givenEnvvars, "NOT_DEFINED", envvars.EnvvarCollection{}},
 	}
-	// when
-	evs := c.WithTag("NOT_THERE")
-	// then
-	assert.Len(t, evs, 0)
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// when
+			got := tc.givenCollection.WithTag(tc.whenTagName)
+			// then
+			if len(got) != len(tc.thenEnvvars) {
+				t.Errorf("want %d, got %d", len(tc.thenEnvvars), len(got))
+				return
+			}
+			for i, envvar := range got {
+				if envvar.Name != tc.thenEnvvars[i].Name {
+					t.Errorf("want %q, got %q", tc.thenEnvvars[i].Name, envvar.Name)
+				}
+			}
+		})
+	}
 }
