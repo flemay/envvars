2dc92440a11670574611d36aef8afd3548b94340
flemay <461102+flemay@users.noreply.github.com>
2021-09-09 22:21:15 +1000

    remove mockery

--- go.mod
+++ go.mod
@@ -2,12 +2,4 @@ module github.com/flemay/envvars
 
 go 1.17
 
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.3.0 // indirect
-	github.com/stretchr/testify v1.7.0
-	gopkg.in/yaml.v2 v2.4.0
-)
-
-require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+require gopkg.in/yaml.v2 v2.4.0
--- pkg/envvars/validate_test.go
+++ pkg/envvars/validate_test.go
@@ -4,14 +4,15 @@ import (
 	"testing"
 
 	"github.com/flemay/envvars/pkg/envvars"
+	"github.com/flemay/envvars/pkg/mock"
 	"github.com/flemay/envvars/pkg/yml"
 )
 
 func TestValidate_toReturnNoErrorIfValidDeclaration(t *testing.T) {
 	// given
-	reader := yml.NewDeclarationYML("testdata/validate_declaration_file.yml")
+	r := yml.NewDeclarationYML("testdata/validate_declaration_file.yml")
 	// when
-	got := envvars.Validate(reader)
+	got := envvars.Validate(r)
 	// then
 	if got != nil {
 		t.Errorf("want no error, got %q", got.Error())
@@ -20,9 +21,9 @@ func TestValidate_toReturnNoErrorIfValidDeclaration(t *testing.T) {
 
 func TestValidate_toReturnNoErrorIfValidDeclarationWithTags(t *testing.T) {
 	// given
-	reader := yml.NewDeclarationYML("testdata/validate_declaration_file_with_tags.yml")
+	r := yml.NewDeclarationYML("testdata/validate_declaration_file_with_tags.yml")
 	// when
-	got := envvars.Validate(reader)
+	got := envvars.Validate(r)
 	// then
 	if got != nil {
 		t.Errorf("want no error, got %q", got.Error())
@@ -31,9 +32,9 @@ func TestValidate_toReturnNoErrorIfValidDeclarationWithTags(t *testing.T) {
 
 func TestValidate_toReturnErrorIfInvalidDeclaration(t *testing.T) {
 	// given
-	reader := yml.NewDeclarationYML("testdata/declaration_file_invalid.yml")
+	r := yml.NewDeclarationYML("testdata/declaration_file_invalid.yml")
 	// when
-	got := envvars.Validate(reader)
+	got := envvars.Validate(r)
 	// then
 	want := readFile(t, "testdata/declaration_file_invalid_error_message.golden")
 	if got.Error() != want {
@@ -43,9 +44,9 @@ func TestValidate_toReturnErrorIfInvalidDeclaration(t *testing.T) {
 
 func TestValidate_toReturnErrorIfDeclarationIsEmpty(t *testing.T) {
 	// given
-	reader := yml.NewDeclarationYML("testdata/declaration_file_empty.yml")
+	r := yml.NewDeclarationYML("testdata/declaration_file_empty.yml")
 	// when
-	got := envvars.Validate(reader)
+	got := envvars.Validate(r)
 	// then
 	want := "declaration must at least have 1 envvars"
 	if got.Error() != want {
@@ -53,13 +54,18 @@ func TestValidate_toReturnErrorIfDeclarationIsEmpty(t *testing.T) {
 	}
 }
 
-// func TestValidate_toReturnErrorIfDeclarationIsNil(t *testing.T) {
-// 	// given
-// 	mockReader := new(mocks.DeclarationReader)
-// 	mockReader.On("Read").Return(nil, nil)
-//
-// 	// when
-// 	err := envvars.Validate(mockReader)
-// 	// then
-// 	assert.EqualError(t, err, "declaration is nil")
-// }
+func TestValidate_toReturnErrorIfDeclarationIsNil(t *testing.T) {
+	// given
+	r := mock.DeclarationReader{}
+	// when
+	got := envvars.Validate(r)
+	// then
+	if got == nil {
+		t.Error("want error, got none")
+		return
+	}
+	want := "declaration is nil"
+	if got.Error() != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
