5dde229ef5ccc38aa1abf5a40c3dcfac0be36717
flemay <461102+flemay@users.noreply.github.com>
2021-09-09 20:22:01 +1000

    replace testify in list_test

--- pkg/envvars/list_test.go
+++ pkg/envvars/list_test.go
@@ -1,10 +1,10 @@
 package envvars_test
 
 import (
+	"testing"
+
 	"github.com/flemay/envvars/pkg/envvars"
 	"github.com/flemay/envvars/pkg/yml"
-	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestList_toReturnAllEnvvarsIfNoTagsSpecified(t *testing.T) {
@@ -13,8 +13,13 @@ func TestList_toReturnAllEnvvarsIfNoTagsSpecified(t *testing.T) {
 	// when
 	c, err := envvars.List(d)
 	// then
-	assert.NoError(t, err)
-	assert.Len(t, c, 3)
+	if err != nil {
+		t.Errorf("want no error, got %q", err.Error())
+		return
+	}
+	if len(c) != 3 {
+		t.Errorf("want number of envvar to be 3, got %d", len(c))
+	}
 }
 
 func TestList_toReturnTaggedEnvvarsIfTagsSpecified(t *testing.T) {
@@ -23,8 +28,13 @@ func TestList_toReturnTaggedEnvvarsIfTagsSpecified(t *testing.T) {
 	// when
 	c, err := envvars.List(reader, "tag1")
 	// then
-	assert.NoError(t, err)
-	assert.Len(t, c, 2)
+	if err != nil {
+		t.Errorf("want no error, got %q", err.Error())
+		return
+	}
+	if len(c) != 2 {
+		t.Errorf("want envvar collection 2, got %d", len(c))
+	}
 }
 
 func TestList_toReturnErrorIfInvalidDeclarationAndTagNameList(t *testing.T) {
@@ -36,7 +46,15 @@ func TestList_toReturnErrorIfInvalidDeclarationAndTagNameList(t *testing.T) {
 	c, err := envvars.List(reader, invalidList...)
 
 	// then
-	expectedErrorMsg := readFile(t, "testdata/declaration_file_with_tag_name_list_invalid_error_message.golden")
-	assert.EqualError(t, err, expectedErrorMsg)
-	assert.Len(t, c, 0)
+	want := readFile(t, "testdata/declaration_file_with_tag_name_list_invalid_error_message.golden")
+	if len(c) != 0 {
+		t.Errorf("want empty envvar collection, got %d", len(c))
+	}
+	if err == nil {
+		t.Errorf("want error %q, got none", want)
+		return
+	}
+	if err.Error() != want {
+		t.Errorf("want error %q, got %q", want, err.Error())
+	}
 }
